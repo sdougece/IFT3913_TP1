3e69372adc3767b6bcc6f2162c9e6a75c84720c4
Yifu Zhou <zhouyvan@gmail.com>
2020-09-26 10:30:53 -0400

    initial parse methode

--- src/Metrics.java
+++ src/Metrics.java
@@ -2,7 +2,8 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Metrics {
     static long classe_LOC = 0;
@@ -12,8 +13,35 @@ public class Metrics {
     static long classe_DC = 0;
     static long methode_DC = 0;
 
+
+    private static void parse(File file) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+
+        } catch (IOException ignored) {
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                }
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
-        File f = new File("test.java");
+        String path = "test.java";
+        System.out.println(path);
+        File f = new File(path);
+        if(path.matches(".*\\.java$")){
+            System.out.println("im a java file");
+            parse(f);
+        }else{
+            System.out.println("not a java file");
+        }
+
     }
 
 }

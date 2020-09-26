b2a52228bd56d921b7e3d9c06a936a2d08fc3e57
Yifu Zhou <zhouyvan@gmail.com>
2020-09-26 09:37:57 -0400

    Metrics.java as main class

---
+++ src/Metrics.java
@@ -0,0 +1,19 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+
+public class Metrics {
+    static long classe_LOC = 0;
+    static long methode_LOC = 0;
+    static long classe_CLOC = 0;
+    static long methode_CLOC = 0;
+    static long classe_DC = 0;
+    static long methode_DC = 0;
+
+    public static void main(String[] args) {
+        File f = new File("test.java");
+    }
+
+}
